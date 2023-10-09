373961a5eb7ed452732d9ba9fc7913a16c60d4c3
javier carrizo <carrizo38@gmail.com>
2023-10-09 19:47:09 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Reproductor from './components/Reproductor/Reproductor';
+
+function App() {
+  return (
+    <div className="App">
+     
+
+<Reproductor/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Reproductor/Reproductor.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Reproductor = () => {
+  return (
+    <div>
+        
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-black'>
+
+
+      
+
+        <div className='mb-6'>
+                 <iframe width="560" height="315" src="https://www.youtube.com/embed/SgIbiba9vEI?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+
+        <div className='mb-6'>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/CgCBGMTQ9tE?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+        <div className='mb-6'>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/HN3ByKwUDr4?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+        <div className='mb-6'>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/jg-LJ9gagO0?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+        <div className='mb-6'>
+
+                 <iframe width="560" height="315" src="https://www.youtube.com/embed/hE7xjmEPmBg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+        <div className='mb-6'>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/BsDz3BxEaSI?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+        <div className='mb-6'>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/6AzIEZmer5s?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+        <div className='mb-6'>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/wBgSH-CGPzg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+        <div className='mb-6'>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/GDQnA1LVCWA?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+        </div>
+
+
+
+        </div>      
+    </div>
+  )
+}
+
+export default Reproductor

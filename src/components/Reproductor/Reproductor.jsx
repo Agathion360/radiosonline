87c58800a1e023a19ef064db2f796dadb3d380c5
javier carrizo <carrizo38@gmail.com>
2023-10-10 10:36:52 -0300

    se cambiaron canales caidos

--- src/components/Reproductor/Reproductor.jsx
+++ src/components/Reproductor/Reproductor.jsx
@@ -1,77 +1,92 @@
 import React from 'react'
 
+
 const Reproductor = () => {
-  return (
-    <div>
-        
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-black'>
-
-
-      
-
-        <div className='mb-6'>
-                 <iframe width="560" height="315" src="https://www.youtube.com/embed/SgIbiba9vEI?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-
-        <div className='mb-6'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/CgCBGMTQ9tE?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/HN3ByKwUDr4?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/jg-LJ9gagO0?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-
-                 <iframe width="560" height="315" src="https://www.youtube.com/embed/hE7xjmEPmBg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/BsDz3BxEaSI?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/6AzIEZmer5s?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-
-                 <iframe width="560" height="315" src="https://www.youtube.com/embed/D944-DEg5Co?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full"></iframe>
-        </div>
-
-
-        <div className='mb-6'>
-                 <iframe width="560" height="315" src="https://www.youtube.com/embed/IWODHs30H6Q?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full"></iframe>
-        </div>
        
-        <div className='mb-6'>
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/wBgSH-CGPzg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/GDQnA1LVCWA?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
-        </div>
-
-        <div className='mb-6'>
-                 <iframe width="560" height="315" src="https://www.youtube.com/embed/sgEJ4sOwboM?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full"></iframe>
-        </div>
-
-
         
 
+     
 
-        </div>      
-    </div>
-  )
+
+        return (
+                <div>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-black'>
+                                {/* radio Rivadavia */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/SgIbiba9vEI?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div>
+                                {/* radio Mitre */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/CgCBGMTQ9tE?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div>
+                                 {/* radio urbana play */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/HN3ByKwUDr4?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div>
+
+                                {/* <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/jg-LJ9gagO0?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div> */}
+
+                                {/*Radio la 100 */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/hE7xjmEPmBg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div>
+
+
+
+                                {/*Radio vorterix */}
+                                <div className='mb-6'>
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/g7Kx0AKmfTg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+
+                                </div>
+                                {/* radio pop */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/BsDz3BxEaSI?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div>
+                                {/* radio heat */}
+                                 <div className='mb-6'>
+                                       <iframe width="560" height="315" src="https://www.youtube.com/embed/pyMj8g_nwoQ?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full"></iframe>
+                                </div> 
+                                {/* radio cadena 3 */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/D944-DEg5Co?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full"></iframe>
+                                </div>
+
+                                {/* tv  france 24 */}
+                                 <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/Y-IlMeCCtIg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full"></iframe>
+                                </div> 
+
+
+                                {/* radio tomorrowland */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/wBgSH-CGPzg?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div>
+                                {/* radio depp house */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/GDQnA1LVCWA?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full" ></iframe>
+                                </div>
+                                {/* radio chillout */}
+                                <div className='mb-6'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/sgEJ4sOwboM?autoplay=1&amp;mute=1&amp;rel=0&amp;modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="w-full"></iframe>
+                                </div>
+
+
+
+
+
+
+
+
+                               
+
+
+
+                        </div>      
+                </div>
+        )
 }
 
-export default Reproductor
+export default Reproductor;
+
